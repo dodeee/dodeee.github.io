bc4f5f3875543f17692ab2f4e18cc09f92805ef1
dodeee <dode.etienne@gmail.com>
2024-01-04 00:05:13 +0100

    styling

--- cards.js
+++ cards.js
@@ -10,17 +10,23 @@ var blackBoxArray = new Array(0);
 
 function initCards(){
 	// define document width and height
-	var width = 1000, height = 40000;
+	var width = 236*3+cardPadding*2, height = 1100;
 
 	// create SVG document and set its size
-	var drawer = SVG().addTo('#card-draw').size(width, height)
+	for(var nb = 0; nb < 7; nb++){
+        var drawer = SVG().addTo('#card-draw').size(width, height)
+        var drawerVerso = SVG().addTo('#card-draw').size(width, height)
+        var size = 20;
+        for(var i = 0; i< 5;i++){
 
-	var size = 20;
-    for(var i = 0; i< 100;i++){
-        var cardArray = createCard(drawer, i, false);
-        var cardArray = createCard(drawer, i, true);
+            var cardArray = createCard(drawer, i, false, 0);
+            var cardArray = createCard(drawerVerso, i, true, 0);
+            var cardArray = createCard(drawer, i, false, 1);
+            var cardArray = createCard(drawerVerso, i, true, 1);
+            var cardArray = createCard(drawer, i, false, 2);
+            var cardArray = createCard(drawerVerso, i, true, 2);
+        }
     }
-    
 	// var cardArray = createCard(drawer, 1, false);
     // var cardArray = createCard(drawer, 1, true);
     //drawCard(cardArray, drawer);
@@ -29,7 +35,7 @@ function initCards(){
 	
 }
 
-function createCard(drawer, cardNb, isVerso){
+function createCard(drawer, cardNb, isVerso, row){
     var greenBoxNb = 0;
     var blackBoxNb = 0;
     while(greenBoxNb < 9 || blackBoxNb < 3){
@@ -71,21 +77,21 @@ function createCard(drawer, cardNb, isVerso){
             }
         }
     }
-    drawCard(cardArray, drawer, cardNb, isVerso);
+    drawCard(cardArray, drawer, cardNb, isVerso, row);
     return cardArray;
 }
 
-function drawCard(cardArray, drawer, cardNb, isVerso){
+function drawCard(cardArray, drawer, cardNb, isVerso, row){
 
     var i = 0;
     var rectY = cardPadding/2 + cardHeight* cardNb + cardNb*cardPadding;
     var rectX = cardPadding/2;
     var rect = drawer.rect(cardWidth, cardHeight).attr({ fill: "rgb(0, 0, 0)"});
     rect.y(cardHeight* cardNb + cardNb*cardPadding);
-    if(isVerso){
-        rectX += cardWidth +cardPadding;
-        rect.x(cardWidth +cardPadding);
-    }
+    
+    rectX += (cardWidth +cardPadding) * row;
+    rect.x((cardWidth +cardPadding) * row);
+    
     rect.radius(10);
     cardArray.forEach(square => {
         if(square == 0){
@@ -115,9 +121,7 @@ function drawCard(cardArray, drawer, cardNb, isVerso){
         rectX += width + padding;
         if((i+1)%5 == 0){
             rectY+=height + padding;
-            rectX = cardPadding/2;
-            if(isVerso)
-                rectX += cardWidth +cardPadding;
+            rectX = cardPadding/2 + (cardWidth +cardPadding) * row;
         }
         i++;
     });
